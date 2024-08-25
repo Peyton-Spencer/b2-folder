8c3ed326184950c7c39b86a83718335fc5422459
Peyton-Spencer <pas1234567@gmail.com>
2024-08-25 15:37:25 -0400

    fix shell completions

--- main.go
+++ main.go
@@ -4,19 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
-	"os/exec"
-	"strings"
-
 	"github.com/peyton-spencer/b2-folder/cmd"
+	"os/exec"
 )
 
 func main() {
-	out, err := exec.Command("b2", "version").Output()
+	_, err := exec.Command("b2", "version").Output()
 	if err != nil {
 		panic("The b2 executable is not available. Please install it.")
 	}
-	outStr := strings.TrimPrefix(string(out), "b2 command line tool, version ")
-	fmt.Printf("Using b2 version %s\n", outStr)
 	cmd.Execute()
 }
